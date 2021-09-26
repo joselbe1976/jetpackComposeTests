b0eeecbe3adeca78a5110491edf79a344b434df3
Jose Luis bustos Esteban <bejl@outlook.es>
2021-09-26 18:23:08 +0200

    Finish video 2

--- App1/app/src/main/java/com/example/app1/MainActivity.kt
+++ App1/app/src/main/java/com/example/app1/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.app1.ui.theme.App1Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,54 +30,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun MediaItem(){
+    Column{
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+                .background(color = Color.Red)
+
+        ){
+            Text("Title 1")
+        }
+    }
+}
+
+
+
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(text = "Hello $name!", modifier = modifier)
 }
-
-//@Preview(showBackground = true, name = "Telefono 1", widthDp = 200, heightDp = 200)
-@Composable
-fun DefaultPreviewBox() {
-    App1Theme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.DarkGray),
-            contentAlignment = Alignment.Center
-        ){
-            Greeting("Android 1")
-            Greeting("Android 2", modifier = Modifier.align(Alignment.BottomEnd))
-        }
-    }
-}
-
-
-@Preview(showBackground = true, name = "Telefono 1", widthDp = 200, heightDp = 200)
-@Composable
-fun DefaultPreview() {
-    App1Theme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            //verticalArrangement = Arrangement.SpaceEvenly //espacio igual en pantalla. Sin pegarse arriba o abajo
-        //    verticalArrangement = Arrangement.SpaceBetween //Como SwiftUI
-         horizontalAlignment = Alignment.CenterHorizontally //centra
-
-        ){
-            Greeting("Android 1", modifier = Modifier
-                .background(Color.Blue)
-                .weight(weight = 2f) //peso en la pantalla
-            )
-            Greeting("Android 2", modifier = Modifier
-                    .background(Color.Yellow)
-                    .weight(weight = 1f) //peso en la pantalla
-            )
-            //Greeting("Android 3", modifier = Modifier.background(Color.Red))
-        }
-    }
-}
-
-
-
 
 
 
