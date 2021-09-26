b24f6b9cda097644d0f6ea951ba01b2af5376b18
Jose Luis bustos Esteban <bejl@outlook.es>
2021-09-26 18:08:19 +0200

    Box

---
+++ App1/app/src/main/java/com/example/app1/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.app1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.app1.ui.theme.App1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+
+@Preview(showBackground = true, name = "Telefono 1", widthDp = 200, heightDp = 200)
+@Composable
+fun DefaultPreview() {
+    App1Theme {
+        Box(
+            modifier = Modifier.fillMaxSize().background(Color.DarkGray),
+            contentAlignment = Alignment.Center
+        ){
+            Greeting("Android 1")
+            Greeting("Android 2", modifier = Modifier.align(Alignment.BottomEnd))
+        }
+    }
+}
